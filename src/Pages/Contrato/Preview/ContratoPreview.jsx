c0843b03e7e59a74e3bbb734337c28ce1f3bbfcb
Fernando Maciel <fernando.maciel80@gmail.com>
2022-11-11 13:42:07 -0300

    Contrato - alt fonte v04

--- src/Pages/Contrato/Preview/ContratoPreview.jsx
+++ src/Pages/Contrato/Preview/ContratoPreview.jsx
@@ -273,8 +273,8 @@ const ContratoPreview = () => {
           <div
             style={{
               textAlign: "center",
-              marginTop: 30,
-              paddingBottom: 30,
+              marginTop: 20,
+              paddingBottom: 20,
               width: "80%",
               display: "flex",
               flexDirection: "column",
